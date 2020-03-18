f7e5772ec06336bad23a2d7b8bfa424d97f9b16d
anish-kmr <anish12k07@gmail.com>
2020-03-18 14:50:54 +0530

    Requirements file modified and Logs removed

--- face_authenticate.sh
+++ face_authenticate.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo  $'\n''User : ' $PAM_USER $'\n''Service : ' $PAM_SERVICE $'\n' $'\n' >> ~/.facelock_logs/logs.log
 timeout 120 python3 /etc/facelock/face_unlock.py
 
 
--- requirements.sh
+++ requirements.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 if [[ "$(python3 -V)" =~ "Python 3" ]]; then
-   	sudo pip3 install numpy
+   	
+	sudo apt-get install build-essential cmake pkg-config
+	sudo apt-get install libx11-dev libatlas-base-dev
+	sudo apt-get install libgtk-3-dev
+	sudo apt-get install libboost-all-dev
+	sudo apt-get install python3-dev
+	sudo pip3 install numpy
     sudo pip3 install opencv-python
-	sudo apt-get -y install build-essential cmake libx11-dev git
-    cd ~
-    mkdir temp
-    cd temp
-    git clone https://github.com/davisking/dlib.git
-    cd dlib
-    mkdir build; cd build; cmake ..; cmake --build .
-    cd ..
-    python3 setup.py install
+	sudo pip3 install dlib
    	sudo pip3 install face_recognition
 else 
     echo "Python3 not installed"
