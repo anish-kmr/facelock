e9a74fb31f72b2ff106a7bd1732cf47770214d27
Anish <anish12k07@gmail.com>
2020-03-25 16:25:22 +0530

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [[ "$(python3 -V)" =~ "Python 3.6" ]]; then
+if [[ "$(python3 -V)" =~ "Python 3" ]]; then
    	
 	sudo pip3 install numpy
     sudo pip3 install opencv-python
     sudo pip3 install opencv-contrib-python
 else 
-    echo "Python3.6 or above not installed"
+    echo "Python3 or above not installed"
 fi
